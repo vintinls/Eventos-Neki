8d6f1d0f47e1e67f0b83e5d09cb2b5404633b681
Victor Hugo Lisboa Machado <victorlsboa@gmail.com>
2025-08-20 16:36:10 -0300

    Revert "#8 - feat(frontend): adicionar estrutura inicial das telas Login e Cadastro de Administrador"
    This reverts commit fa855e31c75bed816c6af8c8eb410c0c477e29d8.

--- frontend/React/web/src/App.tsx
+++ frontend/React/web/src/App.tsx
@@ -1,7 +1,11 @@
-import AppRoutes from './routes/AppRoutes';
-
 function App() {
-  return <AppRoutes />;
+  return (
+    <div className='flex items-center justify-center min-h-screen bg-gray-100'>
+      <h1 className='text-4xl font-bold text-blue-600'>
+        Tailwind funcionando! 🚀
+      </h1>
+    </div>
+  );
 }
 
 export default App;
